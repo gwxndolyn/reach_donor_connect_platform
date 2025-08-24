4d436098b544ed541184d823dabf5ff2d99e9164
kohkakohla <ivanjerrickkoh@gmail.com>
2025-08-25 00:26:03 +0800

    Temporarily disable linting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "models.readyplayer.me" }],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors. This is not recommended unless you
+    // have ESLint configured to run in a separate part of your workflow (for example, in CI).
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
